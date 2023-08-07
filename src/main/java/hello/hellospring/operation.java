d998eff9246d90cc98d2867b2655a4e92201ad0d
millejuice <potgusdavy@gmail.com>
2023-08-07 10:20:47 +0900

    static web개발

---
+++ src/main/java/hello/hellospring/operation.java
@@ -0,0 +1,11 @@
+package hello.hellospring;
+
+public class operation {
+    //java server 터미널에서 시작하는 방법
+    //터미널에서 해당 폴더로 이동
+    //ls -al하면 gradlew라는 파일이 있다
+    // ./gradlew build하면 빌드 폴더가 생성됨
+    //build폴더로 가서 ls + cd libs
+    //snapshot.jar이라는 파일이 있는데
+    //java -jar 파일이름.jar하면 서버 작동
+}
